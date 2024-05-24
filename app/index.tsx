b5398d549d10ea7a40d9d4d90963aff3c4850fa1
GabrielModog <modog.dev@gmail.com>
2024-05-24 14:09:05 -0300

    [refactor] Change old list to list components

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,87 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+
+import { useRouter } from "expo-router";
+
+import { Colors } from "@/constants/colors";
+import useRepositories from "@/hooks/useRepositories";
+import RepositoryList from "@/components/repositories/RepositoryList";
+import { HandleNavigationFunc } from "@/types";
 
 export default function Index() {
+  const router = useRouter();
+
+  const { repositories, handleEndReached, fetching } = useRepositories();
+
+  const handleNavigation: HandleNavigationFunc = ({
+    id,
+    repositoryName,
+    username,
+  }) => {
+    router.push({
+      pathname: `/repository/${id}`,
+      params: {
+        repositoryName: repositoryName,
+        username: username,
+      },
+    });
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>
+          Melhores repositórios do Github ⭐️
+        </Text>
+        <Text style={styles.headerDescription}>
+          Explore os melhores repositórios
+        </Text>
+      </View>
+      <View style={styles.contentContainer}>
+        <RepositoryList
+          data={repositories}
+          fetching={fetching}
+          handleEndReached={handleEndReached}
+          handleNavigation={handleNavigation}
+        />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: Colors.background,
+  },
+  header: {
+    height: 132,
+    justifyContent: "space-between",
+    alignSelf: "stretch",
+    backgroundColor: "black",
+    rowGap: 4,
+    paddingHorizontal: 32,
+    paddingVertical: 16,
+  },
+  headerTitle: {
+    fontSize: 28,
+    fontWeight: "800",
+    color: Colors.background,
+  },
+  headerDescription: {
+    fontSize: 14,
+    fontWeight: "400",
+    color: Colors.lightgray,
+  },
+  contentContainer: {
+    flex: 1,
+    alignSelf: "stretch",
+    marginTop: 42,
+  },
+  list: {
+    paddingBottom: 60,
+    marginHorizontal: 26,
+  },
+});
---
+++ app/repository/[id].tsx
@@ -0,0 +1,74 @@
+import { useLocalSearchParams, useNavigation, useRouter } from "expo-router";
+import { openBrowserAsync } from "expo-web-browser";
+import { StyleSheet, View } from "react-native";
+
+import PRCounter from "@/components/pull-requests/PRCounter";
+
+import { Colors } from "@/constants/colors";
+import usePullRequests from "@/hooks/usePullRequests";
+import BackButton from "@/components/BackButton";
+import PullRequestList from "@/components/pull-requests/PullRequestList";
+
+export default function Repository() {
+  const navigation = useNavigation();
+  const router = useRouter();
+  const { repositoryName, username } = useLocalSearchParams();
+
+  const { pullRequests, repositoryInfo, handleEndReached, fetching } =
+    usePullRequests({
+      repositoryName: repositoryName as string,
+      username: username as string,
+    });
+
+  const handleGoBackNavigation = () => navigation.goBack();
+
+  return (
+    <View style={styles.container}>
+      <BackButton
+        title={repositoryName as string}
+        handleGoBack={handleGoBackNavigation}
+      />
+      <View style={styles.header}>
+        {repositoryInfo && (
+          <PRCounter
+            open={repositoryInfo.open.totalCount}
+            closed={repositoryInfo.closed.totalCount}
+          />
+        )}
+      </View>
+      <View style={styles.content}>
+        <PullRequestList
+          data={pullRequests}
+          handleEndReached={handleEndReached}
+          fetching={fetching}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.background,
+  },
+  header: {
+    position: "static",
+    zIndex: 10,
+  },
+  content: {
+    alignSelf: "stretch",
+    backgroundColor: "transparent",
+    marginVertical: 8,
+  },
+  list: {
+    paddingBottom: 80,
+    marginBottom: 20,
+    marginHorizontal: 26,
+  },
+  loading: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
