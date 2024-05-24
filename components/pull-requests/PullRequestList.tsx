ac5e7614639ec2bdab7eaddc3186431ea5cced1e
GabrielModog <modog.dev@gmail.com>
2024-05-24 14:07:54 -0300

    [feat] Add list components

---
+++ components/pull-requests/PullRequestList.tsx
@@ -0,0 +1,66 @@
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { openBrowserAsync } from "expo-web-browser";
+
+import { Colors } from "@/constants/colors";
+import { IPullRequest } from "@/types";
+import PullRequest from "./PullRequest";
+
+interface PullRequestListProps {
+  data: any[];
+  fetching: boolean;
+  handleEndReached: () => void;
+}
+
+interface PullRequestItemProps extends IPullRequest {}
+
+export default function PullRequestList(props: PullRequestListProps) {
+  const { data, fetching = false, handleEndReached } = props;
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      contentContainerStyle={styles.list}
+      onEndReached={handleEndReached}
+      ListFooterComponent={() =>
+        fetching && <ActivityIndicator size="large" color={Colors.tint} />
+      }
+      renderItem={({ item }) => <PullRequestItem {...item} />}
+    />
+  );
+}
+
+function PullRequestItem(props: PullRequestItemProps) {
+  const { id, title, username, description, createdAt, author, imageUrl, url } =
+    props;
+  return (
+    <TouchableOpacity
+      style={styles.touchable}
+      onPress={() => openBrowserAsync(url)}
+    >
+      <PullRequest
+        id={id}
+        title={title}
+        description={description}
+        username={username}
+        author={author}
+        imageUrl={imageUrl}
+        createdAt={createdAt}
+      />
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  touchable: {
+    flex: 1,
+  },
+  list: {
+    paddingBottom: 120,
+    marginHorizontal: 26,
+  },
+});
---
+++ components/repositories/RepositoryList.tsx
@@ -0,0 +1,86 @@
+import {
+  ActivityIndicator,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+
+import { Colors } from "@/constants/colors";
+import { HandleNavigationFunc, IRepository } from "@/types";
+import Repository from "./Repository";
+
+interface RepositoryListProps {
+  data: any[];
+  fetching: boolean;
+  handleNavigation: HandleNavigationFunc;
+  handleEndReached: () => void;
+}
+
+interface RepositoryItemProps extends IRepository {
+  handleNavigation: HandleNavigationFunc;
+}
+
+export default function RepositoryList(props: RepositoryListProps) {
+  const { data, fetching = false, handleNavigation, handleEndReached } = props;
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      contentContainerStyle={styles.list}
+      onEndReached={handleEndReached}
+      ListFooterComponent={() =>
+        fetching && <ActivityIndicator size="large" color={Colors.tint} />
+      }
+      renderItem={({ item }) => (
+        <RepositoryItem {...item} handleNavigation={handleNavigation} />
+      )}
+    />
+  );
+}
+
+function RepositoryItem(props: RepositoryItemProps) {
+  const {
+    id,
+    title,
+    username,
+    description,
+    forks,
+    stars,
+    author,
+    imageUrl,
+    handleNavigation,
+  } = props;
+  return (
+    <TouchableOpacity
+      style={styles.touchable}
+      onPress={() =>
+        handleNavigation({
+          id: id,
+          repositoryName: title,
+          username: username,
+        })
+      }
+    >
+      <Repository
+        id={id}
+        title={title}
+        description={description}
+        forks={forks}
+        stars={stars}
+        username={username}
+        author={author}
+        imageUrl={imageUrl}
+      />
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  touchable: {
+    flex: 1,
+  },
+  list: {
+    paddingBottom: 60,
+    marginHorizontal: 26,
+  },
+});
